47850debd4fd576281b84940a14b06cebc8a8e82
Qi Ruijingling <142397749+qway70@users.noreply.github.com>
2024-04-06 13:54:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -87,8 +87,9 @@ def music():
 
 @app.route("/music_generator",methods=["GET","POST"])
 def music_generator():
-    q = request.form.get('q')
-    r = replicate.run(    "meta/musicgen:7be0f12c54a8d033a0fbd14418c9af98962da9a86f5ff7811f9b3423a1f0b7d7",
+    q = request.form.get("q")
+    r = replicate.run(    
+        "meta/musicgen:7be0f12c54a8d033a0fbd14418c9af98962da9a86f5ff7811f9b3423a1f0b7d7",
         input={
             "prompt": q,
             "duration": 5
